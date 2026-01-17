81226a8bc628479af9ffad697a80f53ab25a4a41
JonathasTelesdeOliviera <jonathasteles777@gmail.com>
2026-01-17 14:25:48 -0300

    Primeiro commit adicionando esqueleto do prjeto

---
+++ src/main/java/com/JonathasTelesDeOlivieira/Challeng_API_Rest_MuralDeTopicos/ChallengApiRestMuralDeTopicosApplication.java
@@ -0,0 +1,13 @@
+package com.JonathasTelesDeOlivieira.Challeng_API_Rest_MuralDeTopicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengApiRestMuralDeTopicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengApiRestMuralDeTopicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JonathasTelesDeOlivieira/Challeng_API_Rest_MuralDeTopicos/ChallengApiRestMuralDeTopicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JonathasTelesDeOlivieira.Challeng_API_Rest_MuralDeTopicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengApiRestMuralDeTopicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
